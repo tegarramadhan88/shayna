c96c779bae1a22270c99928a1b0b49a7d7ad28dc
tegarramadhan88 <tegar.ramadhan@gmail.com>
2021-07-27 02:50:54 +0700

    First Commit

---
+++ src/components/untitled.vue
@@ -0,0 +1,41 @@
+<template>
+  <carousel
+    :nav="false"
+    :dots="false"
+    :loop="true"
+    :autoplay="true"
+    class="product-thumbs-track ps-slider"
+  >
+    <div
+      class="pt"
+      @click="changeImage(thumbs[0])"
+      :class="thumbs[0] == photo ? 'active' : ''"
+    >
+      <img src="img/mickey1.jpg" alt="" />
+    </div>
+
+    <div
+      class="pt"
+      @click="changeImage(thumbs[1])"
+      :class="thumbs[1] == photo ? 'active' : ''"
+    >
+      <img src="img/mickey2.jpg" alt="" />
+    </div>
+
+    <div
+      class="pt"
+      @click="changeImage(thumbs[2])"
+      :class="thumbs[2] == photo ? 'active' : ''"
+    >
+      <img src="img/mickey3.jpg" alt="" />
+    </div>
+
+    <div
+      class="pt"
+      @click="changeImage(thumbs[3])"
+      :class="thumbs[3] == photo ? 'active' : ''"
+    >
+      <img src="img/mickey4.jpg" alt="" />
+    </div>
+  </carousel>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <header-shayna />
+    <hero-carousel />
+    <women-banner />
+    <insta-shayna />
+    <partner-shayna />
+    <footer-shayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "../components/HeaderShayna.vue";
+import HeroCarousel from "../components/HeroCarousel.vue";
+import InstaShayna from "../components/InstaShayna.vue";
+import PartnerShayna from "../components/PartnerShayna.vue";
+import WomenBanner from "../components/WomenBanner.vue";
+import FooterShayna from "../components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroCarousel,
+    WomenBanner,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
+</script>
